f7e341b27b2928c913e6c43a67f21a3f9b9e8a99
Lee Siang Yin <siangyin916@gmail.com>
2021-10-27 17:06:32 +0800

    first commit for 01 birthday reminder code exercise

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+	return (
+		<main>
+			<section className="container">
+				<h3>0 birthday</h3>
+				<List />
+				<button>Clear All</button>
+			</section>
+		</main>
+	);
 }
 
 export default App;
