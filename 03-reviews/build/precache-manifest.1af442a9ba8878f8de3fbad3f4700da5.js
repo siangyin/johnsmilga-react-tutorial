91047b8905d2b0036e6fa6acdd18981a99fbea09
Lee Siang Yin <siangyin916@gmail.com>
2021-10-29 20:48:41 +0800

    build reviews site

---
+++ 03-reviews/build/precache-manifest.1af442a9ba8878f8de3fbad3f4700da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eeaeff17b16e122e6da98337e71e613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bdc8aa85e4f5d31f465",
+    "url": "/static/css/main.684b11a9.chunk.css"
+  },
+  {
+    "revision": "f7fb305b7b6ddebc7fad",
+    "url": "/static/js/2.50697238.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.50697238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bdc8aa85e4f5d31f465",
+    "url": "/static/js/main.775760c2.chunk.js"
+  },
+  {
+    "revision": "213f644ba03090c9aebc",
+    "url": "/static/js/runtime-main.2a04508f.js"
+  }
+]);
