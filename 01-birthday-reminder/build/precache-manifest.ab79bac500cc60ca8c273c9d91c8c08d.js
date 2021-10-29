a79e3ab59fabec85d20bf19ebe9e450a8cdf5894
Lee Siang Yin <siangyin916@gmail.com>
2021-10-29 16:14:49 +0800

    cleanup

---
+++ 01-birthday-reminder/build/precache-manifest.ab79bac500cc60ca8c273c9d91c8c08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a93486a45d3508963c229997283efee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f514c3fb40329d42c3c",
+    "url": "./static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "3bdc1d8518940277ff32",
+    "url": "./static/js/2.ab0cd556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ab0cd556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f514c3fb40329d42c3c",
+    "url": "./static/js/main.616de4c3.chunk.js"
+  },
+  {
+    "revision": "5ed235daee148742a595",
+    "url": "./static/js/runtime-main.e7e02211.js"
+  }
+]);
