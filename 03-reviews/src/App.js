9dbfdb93f22c2a5dad23222d5879cbcf5f32c791
Lee Siang Yin <siangyin916@gmail.com>
2021-10-29 19:56:46 +0800

    started 03 reviews

---
+++ 03-reviews/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Review from "./Review";
+import { FaGithubSquare } from "react-icons/fa";
+function App() {
+	return (
+		<div>
+			<h2>
+				reviews project setup
+				<FaGithubSquare />
+			</h2>
+		</div>
+	);
+}
+
+export default App;
