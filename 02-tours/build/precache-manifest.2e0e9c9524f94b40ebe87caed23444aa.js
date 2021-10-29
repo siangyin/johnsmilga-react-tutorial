3d8bc9904969f9cee299a499690a553d26ab5ca4
Lee Siang Yin <siangyin916@gmail.com>
2021-10-29 16:15:40 +0800

    npm run build

---
+++ 02-tours/build/precache-manifest.2e0e9c9524f94b40ebe87caed23444aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c392191a78667bf89ef6a3a4beee05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24716df017602bdfd314",
+    "url": "./static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "941abdcaca8828062eff",
+    "url": "./static/js/2.94641d43.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.94641d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24716df017602bdfd314",
+    "url": "./static/js/main.e13b8acf.chunk.js"
+  },
+  {
+    "revision": "c201d7b4905aeadf1b8d",
+    "url": "./static/js/runtime-main.7ed14a38.js"
+  }
+]);
