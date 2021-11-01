9178324d7691e64c547d76989a1e8d335711fc4b
Lee Siang Yin <siangyin916@gmail.com>
2021-11-01 14:05:50 +0800

    npm build

---
+++ 05-menu/build/precache-manifest.6998f13a05550f84583f155fdc18d4e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e310db1c8247068918abf995692b7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ec6a71e69163ae101a",
+    "url": "/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "156774fbb0af34253254",
+    "url": "/static/js/2.b6405399.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b6405399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ec6a71e69163ae101a",
+    "url": "/static/js/main.9536057d.chunk.js"
+  },
+  {
+    "revision": "1afa52b20bc74a1b9529",
+    "url": "/static/js/runtime-main.bad5c0cc.js"
+  }
+]);
