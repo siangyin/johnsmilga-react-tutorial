831c8c058683475bd0cef91c0a8aa45c937d125d
Lee Siang Yin <siangyin916@gmail.com>
2021-11-01 16:27:46 +0800

    updated build

---
+++ 06-tabs/build/precache-manifest.ab8f3d92576000b5b7102cb20bbdf6bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97232bb5b059c18e2a7ab886af276b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4ab1887bfd4626c128",
+    "url": "/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "4e5a4ee3721bde58af23",
+    "url": "/static/js/2.ddb039ba.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ddb039ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4ab1887bfd4626c128",
+    "url": "/static/js/main.b1ba1da4.chunk.js"
+  },
+  {
+    "revision": "5fe3e850d0397ce6cb4e",
+    "url": "/static/js/runtime-main.a62e8ed7.js"
+  }
+]);
