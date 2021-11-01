a792c1eaf736f1848dc1380041631e58882cbb06
Lee Siang Yin <siangyin916@gmail.com>
2021-11-01 11:44:34 +0800

    filter category and create array for unique category from data

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -2,10 +2,23 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+//get unique categories
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+//will result : ['all', 'breakfast', 'lunch', 'shakes']
 
 function App() {
 	const [menuItems, setMenuItems] = useState(items);
 	const [categories, setCategories] = useState([]);
+
+	const filterItems = (category) => {
+		if (category === "all") {
+			setMenuItems(items);
+			return;
+		}
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
+
 	return (
 		<main>
 			<section className="menu-selection">
@@ -13,8 +26,8 @@ function App() {
 					<h2>our menu</h2>
 					<div className="underline"></div>
 				</div>
-				<Categories />
-        <Menu items={ menuItems}/>
+				<Categories filterItems={filterItems} />
+				<Menu items={menuItems} />
 			</section>
 		</main>
 	);
