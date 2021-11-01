81768ec1a008d242cd147f5dc47559653508b3f7
Lee Siang Yin <siangyin916@gmail.com>
2021-11-01 17:19:07 +0800

    added slider click function

---
+++ 07-slider/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+	const [people, setPeople] = useState(data);
+	const [index, setIndex] = useState(0);
+
+	console.log(data);
+	return (
+		<section className="section">
+			<div className="title">
+				<h2>
+					<span>/</span>reviews
+				</h2>
+			</div>
+			<div className="section-center">
+				{people.map((person, i) => {
+					const { id, name, image, title, quote } = person;
+
+					let position = "nextSlide";
+
+					if (i === index) {
+						position = "activeSlide";
+					}
+
+					if (i === index - 1 || (index === 0 && i === people.length - 1)) {
+						position = "lastSlide";
+					}
+
+					return (
+						<article key={id} className={position}>
+							<img src={image} alt={name} className="person-img"></img>
+							<h4>{name}</h4>
+							<p className="title">{title}</p>
+							<p className="text">{quote}</p>
+							<FaQuoteRight className="icon" />
+						</article>
+					);
+				})}
+				<button className="prev" onClick={() => setIndex(index - 1)}>
+					<FiChevronLeft />
+				</button>
+				<button className="next" onClick={() => setIndex(index + 1)}>
+					<FiChevronRight />
+				</button>
+			</div>
+		</section>
+	);
+}
+
+export default App;
