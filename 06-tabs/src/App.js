87f18dc2f18e35043cac37fc5212c45db7d13f2a
Lee Siang Yin <siangyin916@gmail.com>
2021-11-01 16:22:30 +0800

    conditional css, added active btn className

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -42,6 +42,7 @@ function App() {
 								onClick={() => {
 									setValue(i);
 								}}
+								className={`job-btn ${i === value && "active-btn"}`}
 							>
 								{job.company}
 							</button>
