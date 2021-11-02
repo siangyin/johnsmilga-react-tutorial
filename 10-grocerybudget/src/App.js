f43288de192e2b2ab1026a79f540c12c57792c2c
Lee Siang Yin <siangyin916@gmail.com>
2021-11-02 18:25:48 +0800

    created states for app

--- 10-grocerybudget/src/App.js
+++ 10-grocerybudget/src/App.js
@@ -1,9 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+	const [name, setName] = useState("");
+	const [list, setList] = useState([]);
+	const [isEditing, setEditing] = useState(false);
+	const [editID, setEditID] = useState(null);
+	const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+	return (
+		<section className="section-center">
+			<div className="grocery-container">
+				<List />
+				<button className="clear-btn">clear items</button>
+			</div>
+		</section>
+	);
 }
 
-export default App
+export default App;
