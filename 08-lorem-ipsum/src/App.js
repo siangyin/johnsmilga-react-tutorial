99254c6bcfd53e0f82903fa4e5fa8a889a8c0aae
Lee Siang Yin <siangyin916@gmail.com>
2021-11-02 13:32:33 +0800

    completed lorem generator form

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,11 +1,47 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		let num = parseInt(count);
+		if (count <= 0) {
+			num = 1;
+		}
+		if (count > 8) {
+			num = 8;
+		}
+		console.log(` ${count} submitted`);
+		setText(data.slice(0, num));
+	}
+	return (
+		<section className="section-center">
+			<h3>Tired of boring lorem ipsum?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="amount">Paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => {
+						setCount(e.target.value);
+					}}
+				></input>
+				<button type="submit" className="btn">
+					generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((item, i) => {
+					return <p key={i}>{item}</p>;
+				})}
+			</article>
+		</section>
+	);
 }
 
 export default App;
