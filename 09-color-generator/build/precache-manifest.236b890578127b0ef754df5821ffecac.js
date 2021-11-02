1486301d4e82edba2e09c75c69cecd880f13909b
Lee Siang Yin <siangyin916@gmail.com>
2021-11-02 17:11:16 +0800

    build 09

---
+++ 09-color-generator/build/precache-manifest.236b890578127b0ef754df5821ffecac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b023670918a5ca45d3583f25315dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c2c8ded6722aff4933e",
+    "url": "/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "5d4cb2262a75f6bd7f71",
+    "url": "/static/js/2.93832a6e.chunk.js"
+  },
+  {
+    "revision": "1584b1d83a9a044a3483bb3c9804e9fe",
+    "url": "/static/js/2.93832a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2c8ded6722aff4933e",
+    "url": "/static/js/main.54542549.chunk.js"
+  },
+  {
+    "revision": "519fe28e478825083c78",
+    "url": "/static/js/runtime-main.b28ef4c8.js"
+  }
+]);
