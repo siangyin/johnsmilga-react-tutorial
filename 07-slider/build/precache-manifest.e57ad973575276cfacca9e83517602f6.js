20bcff27f91514c1741f66b02f659304a23b61c8
Lee Siang Yin <siangyin916@gmail.com>
2021-11-02 10:02:14 +0800

    build 07 slider site

---
+++ 07-slider/build/precache-manifest.e57ad973575276cfacca9e83517602f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5deb7bbd8164d2b0479bf1cb2198489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adbe1b919af34110b4f1",
+    "url": "/static/css/main.70c55651.chunk.css"
+  },
+  {
+    "revision": "4788f873721d9e5e28d7",
+    "url": "/static/js/2.a4895b7d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a4895b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbe1b919af34110b4f1",
+    "url": "/static/js/main.880eceae.chunk.js"
+  },
+  {
+    "revision": "7cfb30f24c3f2dd91e98",
+    "url": "/static/js/runtime-main.17d8e888.js"
+  }
+]);
--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -6,7 +6,23 @@ function App() {
 	const [people, setPeople] = useState(data);
 	const [index, setIndex] = useState(0);
 
-	console.log(data);
+	useEffect(() => {
+		const lastIdx = people.length - 1;
+		if (index < 0) {
+			setIndex(lastIdx);
+		}
+		if (index > people.length - 1) {
+			setIndex(0);
+		}
+	}, [index, people]);
+
+	useEffect(() => {
+		let slider = setInterval(() => {
+			setIndex(index + 1);
+		}, 3000);
+		return () => clearInterval(slider);
+	}, [index]);
+
 	return (
 		<section className="section">
 			<div className="title">
