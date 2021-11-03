9fb99cd11462a22ccf6f72ef79f19cdab7cf2216
Lee Siang Yin <siangyin916@gmail.com>
2021-11-03 20:52:04 +0800

    iterate the navbar (header,footer) using data.js

---
+++ 12-sidebar-modal/src/Sidebar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+
+const Sidebar = () => {
+	return (
+		<aside className={`sidebar show-sidebar`}>
+			<div className="sidebar-header">
+				<img className="logo" src={logo} alt="logo"></img>
+				<button className="close-btn">
+					<FaTimes />
+				</button>
+			</div>
+			<ul className="links">
+				{links.map((item) => {
+					const { id, url, text, icon } = item;
+					return (
+						<li key={id}>
+							<a href={url}>
+								{icon}
+								{text}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+
+			<ul className="social-icons">
+				{social.map((item) => {
+					const { id, url, icon } = item;
+					return (
+						<li key={id}>
+							<a href={url}>{icon}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</aside>
+	);
+};
+
+export default Sidebar;
