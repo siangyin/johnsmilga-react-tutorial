f39f8aab35379b57684d8ebd8773a8be388492b9
Lee Siang Yin <siangyin916@gmail.com>
2021-11-03 19:10:52 +0800

    dynamic links height using
    current.getBoundingClientRect().height;

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,18 +4,35 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+	const [showLinks, setShowLinks] = useState(false);
+	const linksContainerRef = useRef(null);
+	const linksRef = useRef(null);
+
+	useEffect(() => {
+		const linkHeight = linksRef.current.getBoundingClientRect().height;
+		// DOMRect {x: 0, y: 79, width: 517, height: 200, top: 79, …}
+		if (showLinks) {
+			linksContainerRef.current.style.height = `${linkHeight}px`;
+		} else {
+			linksContainerRef.current.style.height = "0px";
+		}
+	}, [showLinks]);
+
 	return (
 		<nav>
 			<div className="nav-center">
 				<div className="nav-header">
 					<img src={logo} alt="logo"></img>
-					<button className="nav-toggle">
+					<button
+						className="nav-toggle"
+						onClick={() => setShowLinks(!showLinks)}
+					>
 						<FaBars />
 					</button>
 				</div>
 
-				<div className="links-container show-container">
-					<ul className="links">
+				<div className="links-container" ref={linksContainerRef}>
+					<ul className="links" ref={linksRef}>
 						{links.map((item) => {
 							const { id, url, text } = item;
 							return (
@@ -26,6 +43,7 @@ const Navbar = () => {
 						})}
 					</ul>
 				</div>
+
 				<ul className="social-icons">
 					{social.map((item) => {
 						const { url, id, icon } = item;
