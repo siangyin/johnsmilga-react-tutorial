d2ca3b0362f9dc65b61d7de92571e10588a43372
Lee Siang Yin <siangyin916@gmail.com>
2021-11-03 17:16:08 +0800

    build 10-grocery

---
+++ 10-grocerybudget/build/precache-manifest.855f7b694a77f74c1c74f9371ebdba86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1d56bd3ec4d443b096982b059b7316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efebc384f87f2e6781f5",
+    "url": "/static/css/main.99e9fbee.chunk.css"
+  },
+  {
+    "revision": "e4f37023decb08d3aae2",
+    "url": "/static/js/2.723ce9b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.723ce9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efebc384f87f2e6781f5",
+    "url": "/static/js/main.10904b4d.chunk.js"
+  },
+  {
+    "revision": "1b8b1cd2367f34af6bcc",
+    "url": "/static/js/runtime-main.f61b5943.js"
+  }
+]);
