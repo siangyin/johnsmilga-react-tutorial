a1c65d7bb25fe35abc8f7ecd25832ec00f0474d8
Lee Siang Yin <siangyin916@gmail.com>
2021-11-03 19:15:59 +0800

    11 navbar build

---
+++ 11-navbar/build/precache-manifest.b18bf12e64a003acef163ee0cb52bf73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6426c5ab5478d2d84d9a4c66c023ba82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e1ca27777bbe1efed5d",
+    "url": "/static/css/main.a3a61292.chunk.css"
+  },
+  {
+    "revision": "b1c2ed1ba1861b35ee05",
+    "url": "/static/js/2.d3a5d8d0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d3a5d8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1ca27777bbe1efed5d",
+    "url": "/static/js/main.c0210cfe.chunk.js"
+  },
+  {
+    "revision": "ba3ef42afe633cd6abe2",
+    "url": "/static/js/runtime-main.7edd85d2.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/static/media/logo.2bb7da65.svg"
+  }
+]);
