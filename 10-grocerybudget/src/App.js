5c79d8627c550109bf0def3670f5541a736b60d2
Lee Siang Yin <siangyin916@gmail.com>
2021-11-03 15:51:35 +0800

    added submit handle

--- 10-grocerybudget/src/App.js
+++ 10-grocerybudget/src/App.js
@@ -9,8 +9,42 @@ function App() {
 	const [editID, setEditID] = useState(null);
 	const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (!name) {
+			// if name not empty display alert
+		} else if (name && isEditing) {
+			// deal with editing
+		} else {
+			// show alert
+		}
+		setList((prev) => {
+			[name, ...prev];
+		});
+		console.log(list);
+	}
 	return (
 		<section className="section-center">
+			<form onSubmit={handleSubmit} className="grocery-form">
+				{alert.show && <Alert />}
+				<h3>grocery budget</h3>
+
+				<div className="form-control">
+					<input
+						type="text"
+						className="grocery"
+						placeholder="e.g. eggs"
+						value={name}
+						onChange={(e) => {
+							setName(e.target.value);
+						}}
+					></input>
+					<button type="submit" className="submit-btn">
+						{isEditing ? "edit" : "submit"}
+					</button>
+				</div>
+			</form>
+
 			<div className="grocery-container">
 				<List />
 				<button className="clear-btn">clear items</button>
