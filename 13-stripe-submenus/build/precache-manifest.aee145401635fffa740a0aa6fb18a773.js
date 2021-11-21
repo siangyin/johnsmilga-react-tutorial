ab58fd758eff122ef60ce3c76ae35f86840a0f76
Lee Siang Yin <siangyin916@gmail.com>
2021-11-21 23:14:24 +0800

    complete p13 stripe submenu

---
+++ 13-stripe-submenus/build/precache-manifest.aee145401635fffa740a0aa6fb18a773.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3597caf95a6b2ab9456bbe89d49e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "712b96b1813af19f1cbe",
+    "url": "/static/css/main.d4ca8e90.chunk.css"
+  },
+  {
+    "revision": "5c74610ae15f648ccfa9",
+    "url": "/static/js/2.0415f4aa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0415f4aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712b96b1813af19f1cbe",
+    "url": "/static/js/main.006af0df.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/static/media/phone.58d7e3d6.svg"
+  }
+]);
