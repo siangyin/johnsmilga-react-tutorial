ea99e53a08f661a377d60a2ba70a34d241f56694
Lee Siang Yin <siangyin916@gmail.com>
2021-11-21 14:50:43 +0800

    install npm and updated Context

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,2 +1,38 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+function AppProvider({ children }) {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	const [isModalOpen, setIsModalOpen] = useState(true);
+
+	function openSidebar() {
+		setIsSidebarOpen(true);
+	}
+	function closeSidebar() {
+		setIsSidebarOpen(false);
+	}
+
+	function openModal() {
+		setIsModalOpen(true);
+	}
+	function closeModal() {
+		setIsModalOpen(false);
+	}
+
+	return (
+		<AppContext.Provider
+			value={{
+				isModalOpen,
+				isSidebarOpen,
+				openModal,
+				closeModal,
+				openSidebar,
+				closeSidebar,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+}
