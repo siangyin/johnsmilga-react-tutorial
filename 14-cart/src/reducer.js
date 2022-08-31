034a51b93d29b6d1a91b2806d5c75561f2c62f47
Lee Siang Yin <siangyin916@gmail.com>
2022-09-01 00:30:09 +0800

    setup file & update useReducer
    setup file, update useReducer, add clearItem & remove item function

---
+++ 14-cart/src/reducer.js
@@ -0,0 +1,12 @@
+export const reducer = (state, action) => {
+	if (action.type === "CLEAR_CART") {
+		return { ...state, cart: [] };
+	}
+	if (action.type === "REMOVE") {
+		return {
+			...state,
+			cart: state.cart.filter((item) => item.id !== action.payload),
+		};
+	}
+	return state;
+};
